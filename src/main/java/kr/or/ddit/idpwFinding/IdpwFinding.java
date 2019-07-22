56c7bc59dc6500a49e9c6e570cb214521393a294
khlee <ghogho67@gmail.com>
2019-07-22 15:36:05 +0900

    다시 커밋

--- src/main/java/kr/or/ddit/idpwFinding/IdpwFinding.java
+++ src/main/java/kr/or/ddit/idpwFinding/IdpwFinding.java
@@ -1,18 +1,14 @@
 package kr.or.ddit.idpwFinding;
 
-import javax.annotation.Resource;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import kr.or.ddit.member.member.service.IMemberService;
 @RequestMapping("/find")
 @Controller
 public class IdpwFinding {
 
-	@Resource(name = "memService")
-	private IMemberService memService;
+//	@Resource(name = "memberService")
+//	private IMemberService memberService;
 	
 	
 	@RequestMapping(path = "/idFind", method = RequestMethod.GET)
