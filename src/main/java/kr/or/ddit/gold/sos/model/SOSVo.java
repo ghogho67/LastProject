faf4f2bb46ad3a101bf431e17490b838763d14ee
khlee <ghogho67@gmail.com>
2019-07-22 11:27:29 +0900

    SOSVo 누락 해결

---
+++ src/main/java/kr/or/ddit/gold/sos/model/SOSVo.java
@@ -0,0 +1,28 @@
+package kr.or.ddit.gold.sos.model;
+
+public class SOSVo {
+	private int sos_id;
+	private int gps_id;
+
+	public int getSos_id() {
+		return sos_id;
+	}
+
+	public void setSos_id(int sos_id) {
+		this.sos_id = sos_id;
+	}
+
+	public int getGps_id() {
+		return gps_id;
+	}
+
+	public void setGps_id(int gps_id) {
+		this.gps_id = gps_id;
+	}
+
+	@Override
+	public String toString() {
+		return "SOSVo [sos_id=" + sos_id + ", gps_id=" + gps_id + "]";
+	}
+
+}
