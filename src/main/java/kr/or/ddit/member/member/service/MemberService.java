c7a8c1849b0912cd7fb50a9597843c53a26ce06c
khlee <ghogho67@gmail.com>
2019-07-22 15:16:35 +0900

    login 설정중

--- src/main/java/kr/or/ddit/member/member/service/MemberService.java
+++ src/main/java/kr/or/ddit/member/member/service/MemberService.java
@@ -1,13 +1,47 @@
 package kr.or.ddit.member.member.service;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
 import kr.or.ddit.member.member.dao.IMemberDao;
+import kr.or.ddit.member.member.model.MemberVo;
+import kr.or.ddit.page.model.PageVo;
 
 @Service
 public class MemberService implements IMemberService {
 	@Resource(name = "memberDao")
 	private IMemberDao memberDao;
+
+	@Override
+	public List<MemberVo> getMemList() {
+		return memberDao.getMemList();
+	}
+
+	@Override
+	public int deleteMem(String mem_id) {
+		return memberDao.deleteMem(mem_id);
+	}
+
+	@Override
+	public MemberVo getMemVo(String mem_id) {
+		return memberDao.getMemVo(mem_id);
+	}
+
+	@Override
+	public int memberCnt() {
+		return 0;
+	}
+
+	@Override
+	public int updateMem(MemberVo memVo) {
+		return 0;
+	}
+
+	@Override
+	public List<MemberVo> memPagingList(PageVo pageVo) {
+		return null;
+	}
 }
--- src/test/java/kr/or/ddit/member/dao/MemberDaoTest.java
+++ src/test/java/kr/or/ddit/member/dao/MemberDaoTest.java
@@ -2,15 +2,21 @@ package kr.or.ddit.member.dao;
 
 import static org.junit.Assert.*;
 
+import javax.annotation.Resource;
+
 import org.junit.Test;
 
+import kr.or.ddit.member.member.dao.IMemberDao;
 import kr.or.ddit.testenv.LogicTestEnv;
 
 public class MemberDaoTest extends LogicTestEnv{
 
+	@Resource(name = "memberDao")
+	private IMemberDao memberDao;
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void getMemList() {
+		
+	
 	}
 
 }
