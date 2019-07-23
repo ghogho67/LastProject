970e12c7dc82968ed9c7c23a3fa49fcfc63f74b5
brightoner <brightoner82@gmail.com>
2019-07-23 22:48:31 +0900

    회원가입 ing

---
+++ src/main/java/kr/or/ddit/register/service/RegisterService.java
@@ -0,0 +1,137 @@
+package kr.or.ddit.register.service;
+
+import javax.annotation.Resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import kr.or.ddit.member.careWorker.career.model.CareerVo;
+import kr.or.ddit.member.careWorker.day.model.DayVo;
+import kr.or.ddit.member.careWorker.hospital.model.HospitalVo;
+import kr.or.ddit.member.careWorker.location.model.LocationVo;
+import kr.or.ddit.member.careWorker.serviceType.model.ServiceTypeVo;
+import kr.or.ddit.member.member.model.MemberVo;
+import kr.or.ddit.member.memberDisease.model.MemberDiseaseVo;
+import kr.or.ddit.register.dao.IRegisterDao;
+
+@Service
+public class RegisterService implements IRegisterService {
+
+	@Resource(name = "registerDao")
+	private IRegisterDao registerDao;
+
+	
+	/**
+	* Method : insertMember
+	* 작성자 : ADMIN
+	* 변경이력 :
+	* @param memVo
+	* @return
+	* Method 설명 : 일반회원 가입
+	*/
+	@Override
+	public int insertMember(MemberVo memberVo) {
+		return registerDao.insertMember(memberVo);
+	}
+
+
+	/**
+	* Method : insertMemberDisease
+	* 작성자 : ADMIN
+	* 변경이력 :
+	* @param memberDiseaseVo
+	* @return
+	* Method 설명 : 일반회원 가입 - 질병명
+	*/
+	@Override
+	public int insertMemberDisease(MemberDiseaseVo memberDiseaseVo) {
+		return registerDao.insertMemberDisease(memberDiseaseVo);
+	}
+
+	//-------------------------------------------------------------------------
+	
+	/**
+	* Method : insertCWMember
+	* 작성자 : ADMIN
+	* 변경이력 :
+	* @param memberVo
+	* @return
+	* Method 설명 :요양보호사 회원가입
+	*/
+	@Override
+	public int insertCWMember(MemberVo memberVo) {
+		return registerDao.insertCWMember(memberVo);
+	}
+
+
+	/**
+	* Method : insertLocation
+	* 작성자 : ADMIN
+	* 변경이력 :
+	* @param locationVo
+	* @return
+	* Method 설명 :요양보호사 등록 - 서비스 지역
+	*/
+	@Override
+	public int insertLocation(LocationVo locationVo) {
+		return registerDao.insertLocation(locationVo);
+	}
+
+	/**
+	* Method : insertDay
+	* 작성자 : ADMIN
+	* 변경이력 :
+	* @param dayVo
+	* @return
+	* Method 설명 :요양보호사 등록 - 가능요일
+	*/
+	@Override
+	public int insertDay(DayVo dayVo) {
+		return registerDao.insertDay(dayVo);
+	}
+
+	/**
+	* Method : insertCareer
+	* 작성자 : ADMIN
+	* 변경이력 :
+	* @param careerVo
+	* @return
+	* Method 설명 :요양보호사 등록 - 경력
+	*/
+	@Override
+	public int insertCareer(CareerVo careerVo) {
+		return registerDao.insertCareer(careerVo);
+	}
+
+	/**
+	* Method : insertHospital
+	* 작성자 : ADMIN
+	* 변경이력 :
+	* @param hospitalVo
+	* @return
+	* Method 설명 :요양보호사 등록 - 경력 - 병원명
+	*/
+	@Override
+	public int insertHospital(HospitalVo hospitalVo) {
+		return registerDao.insertHospital(hospitalVo);
+	}
+
+
+	/**
+	* Method : insertServiceType
+	* 작성자 : ADMIN
+	* 변경이력 :
+	* @param serviceTypeVo
+	* @return
+	* Method 설명 :요양보호사 증록 - 서비스 종류
+	*/
+	@Override
+	public int insertServiceType(ServiceTypeVo serviceTypeVo) {
+		return registerDao.insertServiceType(serviceTypeVo);
+	}
+
+
+
+
+}
--- src/main/java/kr/or/ddit/util/RegCheck.java
+++ src/main/java/kr/or/ddit/util/RegCheck.java
@@ -1,10 +1,104 @@
 package kr.or.ddit.util;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 //정규식 체크 
 public class RegCheck {
 	
+	//정규식이 통과하면 true, 틀리면 false
 	private boolean result = false;
+	
+	/**
+	* Method : regId
+	* 작성자 : ADMIN
+	* 변경이력 :
+	* @param mem_id
+	* @return
+	* Method 설명 : 아이디 정규식 - 영어대소문자, 숫자, 특수문자 4~8자리
+	*/
+	public boolean regId(String mem_id) {
+		Pattern p = Pattern.compile("^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!%*#?&])[A-Za-z[0-9]$@$!%*#?&]{4,8}$");
+		Matcher m = p.matcher(mem_id);
+		result = m.matches();
+		return result;
+	}
+	
+	
+	/**
+	* Method : regPass
+	* 작성자 : ADMIN
+	* 변경이력 :
+	* @param mem_pass
+	* @return
+	* Method 설명 : 영어, 숫자  4~6자리
+	*/
+	public boolean regPass(String mem_pass) {
+		Pattern p = Pattern.compile("^[a-zA-Z0-9]{4,6}$");
+		Matcher m = p.matcher(mem_pass);
+		result = m.matches();
+		return result;
+	}
+	
+	/**
+	* Method : regName
+	* 작성자 : ADMIN
+	* 변경이력 :
+	* @param mem_nm
+	* @return
+	* Method 설명 : 이름 정규식 - 한글 2~5자리
+	*/
+	public boolean regName(String mem_nm) {
+		Pattern p = Pattern.compile("^[가-힣]{2,5}$");
+		Matcher m = p.matcher(mem_nm);
+		result = m.matches();
+		return result;
+	}
+	
+	/**
+	* Method : regPhone
+	* 작성자 : ADMIN
+	* 변경이력 :
+	* @param mem_phone
+	* @return
+	* Method 설명 :핸드폰 정규식
+	*/
+	public boolean regPhone(String mem_phone) {
+		Pattern p = Pattern.compile("/^\\\\d{3}-\\\\d{3,4}-\\\\d{4}$/");
+		Matcher m = p.matcher(mem_phone);
+		result = m.matches();
+		return result;
+	}
+	
+	/**
+	* Method : regMail
+	* 작성자 : ADMIN
+	* 변경이력 :
+	* @param mem_mail
+	* @return
+	* Method 설명 :이메일 정규식
+	*/
+	public boolean regMail(String mem_mail) {
+		Pattern p = Pattern.compile("^[_a-zA-Z0-9-\\\\.]+@[\\\\.a-zA-Z0-9-]+\\\\.[a-zA-Z]+$");
+		Matcher m = p.matcher(mem_mail);
+		result = m.matches();
+		return result;
+	}
+	
+	
 	
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
