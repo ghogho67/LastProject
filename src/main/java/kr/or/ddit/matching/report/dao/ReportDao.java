ad08b5a7f3fae9cbddfc9df8cb5e5c1a062d16fb
요한 <>
2019-08-23 03:35:42 +0900

    maps 수정 map관련 이미지 추가

--- src/main/java/kr/or/ddit/matching/report/dao/ReportDao.java
+++ src/main/java/kr/or/ddit/matching/report/dao/ReportDao.java
@@ -16,57 +16,45 @@ import kr.or.ddit.matching.report.model.ReportWriteVo;
 
 @Repository
 public class ReportDao implements IReportDao {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(ReportDao.class);
-	
+
 	@Resource(name = "sqlSession")
 	private SqlSessionTemplate sqlSession;
-	
-	
-	
 
 	@Override
 	public List<ReportVo> getReportList(String mat_id) {
-		return sqlSession.selectList("matchingReport.getMatchingList",mat_id);
+		return sqlSession.selectList("matchingReport.getMatchingList", mat_id);
 	}
-
-
 
 //	@Override
 //	public ReportVo getReport(String rep_id) {
 //		return sqlSession.selectOne("matchingReport.getMatching",rep_id);
 //	}
 
-
-
 	@Override
 	public List<MatchingReportVo> getAllReportList(String mem_id) {
-		return sqlSession.selectList("matchingReport.getAllReportList",mem_id);
+		return sqlSession.selectList("matchingReport.getAllReportList", mem_id);
 	}
-
-
 
 	@Override
-	public MatchingReportAttachmentVo getCertainReportWA (MatchingReportAttachmentVo matchingReportAttachmentVo) {
-		return sqlSession.selectOne("matchingReport.getCertainReportWA",matchingReportAttachmentVo);
+	public MatchingReportAttachmentVo getCertainReportWA(MatchingReportAttachmentVo matchingReportAttachmentVo) {
+		return sqlSession.selectOne("matchingReport.getCertainReportWA", matchingReportAttachmentVo);
 	}
-
-
 
 	@Override
 	public MatchingReportVo getCertainReport(MatchingReportVo matchingReportVo) {
-		return sqlSession.selectOne("matchingReport.getCertainReport",matchingReportVo);
+		return sqlSession.selectOne("matchingReport.getCertainReport", matchingReportVo);
 	}
-	
 
 	@Override
 	public List<MatchingReportVo> getWorkerReportList(String cw_mem_id) {
-		return sqlSession.selectList("matchingReport.getWorkerReportList",cw_mem_id);
+		return sqlSession.selectList("matchingReport.getWorkerReportList", cw_mem_id);
 	}
 
 	@Override
 	public MatchingReportAttachmentVo getWorkerCertainReportWA(MatchingReportAttachmentVo matchingReportAttachmentVo) {
-		return sqlSession.selectOne("matchingReport.getWorkerCertainReportWA",matchingReportAttachmentVo);
+		return sqlSession.selectOne("matchingReport.getWorkerCertainReportWA", matchingReportAttachmentVo);
 	}
 
 	@Override
@@ -74,51 +62,35 @@ public class ReportDao implements IReportDao {
 		return sqlSession.insert("report.insert", rwv);
 	}
 
-	
 	@Override
 	public ReportVo getReportVo(int mat_id) {
-		
-		return sqlSession.selectOne("report.getReportVo",mat_id);
+
+		return sqlSession.selectOne("report.getReportVo", mat_id);
 
 	}
 
 	@Override
 	public ReportVo getLatestReport() {
-		
+
 		return sqlSession.selectOne("report.getLatestReport");
-		
+
 	}
-
-
 
 	@Override
 	public int reportWrite(ReportVo reportVo) {
-		logger.debug("☞reportVo:{}",reportVo);
-		return sqlSession.insert("report.reportWrite",reportVo);
+		logger.debug("☞reportVo:{}", reportVo);
+		return sqlSession.insert("report.reportWrite", reportVo);
 	}
-
-
 
 	@Override
 	public int reportModify(ReportVo reportVo) {
 		return sqlSession.update("report.reportModify", reportVo);
 	}
 
-
-
 //	@Override
 //	public ReportVo getReport(int rep_id) {
 //		
 //		return sqlSession.selectOne("report.getReport",rep_id);
 //	}
-	
 
 }
-
-
-
-
-
-
-
-
