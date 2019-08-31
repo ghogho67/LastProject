98cedc3725930cc8102771cd49c93b6288e670c6
Minchangs <bijou0147@gmail.com>
2019-08-31 13:50:40 +0900

    1

--- src/main/java/kr/or/ddit/login/controller/LoginController.java
+++ src/main/java/kr/or/ddit/login/controller/LoginController.java
@@ -199,7 +199,6 @@ public class LoginController {
 	@RequestMapping(path = "/gradeChangeToGold" )
 	public String goldmemberJoin(HttpSession session ,ApprovalVo approvalVo) {
 		
-		
 		MemberVo memberVo = (MemberVo) session.getAttribute("MEM_INFO");
 		String mem_id=memberVo.getMem_id();
 		logger.debug("@@@@mem_id{}",mem_id);
